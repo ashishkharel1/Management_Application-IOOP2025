3ebd9d3d65abff541302802a0ec76c5b80e46bbf
Your Name <you@example.com>
2025-10-24 19:34:55 -0400

    test123

--- src/db/HashPw/HashPw.java
+++ src/db/HashPw/HashPw.java
@@ -1,7 +1,7 @@
 import org.mindrot.jbcrypt.BCrypt;
 public class HashPw {
   public static void main(String[] args) {
-    String pw = args.length > 0 ? args[0] : "demo123!";
+    String pw = args.length > 0 ? args[0] : "demo123!"; //test123
     String hash = BCrypt.hashpw(pw, BCrypt.gensalt(12));
     System.out.println(hash);
   }
